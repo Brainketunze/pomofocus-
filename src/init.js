05804b8f5466984febf509062a8786393efcdd62
Ketunze Brain <brainketunze@gmail.com>
2021-02-08 18:28:09 +0100

    init.js file

--- src/init.js
+++ src/init.js
@@ -5,6 +5,8 @@
 // does your project have code that executes when the document or window are ready?
 // that code can go here
 
+console.log('hello');
+
 
 import { Timer } from './timer.js'
 import { Task } from './task.js'
